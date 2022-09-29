da40481a43b29f47885355dc680b3e6e7de1ad73
fabiosvt1 <fabio.silvestri@bairesdev.com>
2022-09-29 18:53:30 -0300

    Initial Commit

---
+++ InfiniteScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteScroll
+//
+//  Created by Fabio Silvestri on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
